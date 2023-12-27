6c9f20dd666e12125712fad9b9f764bdd2f219c0
MarcosDantasdeSantana <billgm2@outlook.com>
2023-12-27 20:55:46 -0300

    [Fix] Correção do rebase do kickstart
    Apaguei uma parte importante sem querer

--- init.lua
+++ init.lua
@@ -607,6 +607,10 @@ local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
