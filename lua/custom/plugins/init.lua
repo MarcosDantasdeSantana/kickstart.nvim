d230b30780aea8b8506d2cdeb33e6ab7566e525b
MarcosDantasdeSantana <billgm2@outlook.com>
2023-12-28 23:06:57 -0300

    [Feat] New Formater
    Conform Nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -88,7 +88,40 @@ return {
     },
   },
 
+  { -- NOTE: Formater: Conform.nvim
+    'stevearc/conform.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local conform = require 'conform'
 
+      conform.setup {
+        formatters_by_ft = {
+          javascript = { 'ast-grep' },
+          typescript = { 'ast-grep' },
+          javascriptreact = { 'ast-grep' },
+          typescriptreact = { 'ast-grep' },
+          css = { 'ast-grep' },
+          html = { 'ast-grep' },
+          json = { 'ast-grep' },
+          markdown = { 'ast-grep' },
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
+      }
+      -- vim.keymap.set({ 'n', 'v' }, '<C-s>', function()
+      --   conform.format {
+      --     lsp_fallback = true,
+      --     async = false,
+      --     timeout_ms = 1000,
+      --   }
+      -- end, { desc = 'Format File' })
+    end,
+  },
 
 
   { -- NOTE: Fold look modern and keep high performance.
