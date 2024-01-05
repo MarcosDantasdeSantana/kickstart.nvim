551716b9a1e417c55c47c9c30001687d1d3a9ed2
MarcosDantasdeSantana <billgm2@outlook.com>
2024-01-05 18:42:16 -0300

    [FIX] LuaLine Dependencies

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return { -- NOTE: Edit statusline
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- See `:help lualine.txt`
   opts = {
     options = {
