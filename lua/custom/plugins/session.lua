74a1344b74eda14d479be06cf65794b1c351c17f
MarcosDantasdeSantana <billgm2@outlook.com>
2024-01-05 18:40:47 -0300

    Sessions Config
    Configurações das sessões

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -1,3 +1,17 @@
 return { -- NOTE: Session Manager
   'Shatur/neovim-session-manager',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    local Path = require 'plenary.path'
+    local config = require 'session_manager.config'
+
+    require('session_manager').setup {
+      -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+      autoload_mode = config.AutoloadMode.Disabled,
+      autosave_last_session = true, -- Automatically save last session on exit and on session switch.
+      autosave_only_in_session = true, -- Always autosaves session. If true, only autosaves after a session is active.
+    }
+  end,
 }
