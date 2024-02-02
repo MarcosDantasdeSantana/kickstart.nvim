1b3babc2dd41a047b607a5f5e20aee3e9c2bd1e4
MarcosDantasdeSantana <billgm2@outlook.com>
2024-02-01 22:32:01 -0300

    Disable Autopairs and Formatter.
    Now i use default Kickstater AutoFormatter

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -1,34 +1,36 @@
 return { -- NOTE: Formater: Conform.nvim
-  'stevearc/conform.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    local conform = require 'conform'
-
-    conform.setup {
-      formatters_by_ft = {
-        javascript = { 'ast-grep' },
-        typescript = { 'ast-grep' },
-        javascriptreact = { 'ast-grep' },
-        typescriptreact = { 'ast-grep' },
-        css = { 'ast-grep' },
-        html = { 'ast-grep' },
-        json = { 'ast-grep' },
-        markdown = { 'ast-grep' },
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    }
-    vim.keymap.set({ 'n', 'v' }, '<leader>df', function()
-      conform.format {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      }
-    end, { desc = '[F]ormat File' })
-  end,
+  -- 'stevearc/conform.nvim',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  -- config = function()
+  --   local conform = require 'conform'
+  --
+  --   conform.setup {
+  --     formatters_by_ft = {
+  --       c = { 'ast-grep' },
+  --       java = { 'ast-grep' },
+  --       javascript = { 'ast-grep' },
+  --       typescript = { 'ast-grep' },
+  --       javascriptreact = { 'ast-grep' },
+  --       typescriptreact = { 'ast-grep' },
+  --       css = { 'ast-grep' },
+  --       html = { 'ast-grep' },
+  --       json = { 'ast-grep' },
+  --       markdown = { 'ast-grep' },
+  --       lua = { 'stylua' },
+  --       python = { 'black' },
+  --     },
+  --     format_on_save = {
+  --       lsp_fallback = true,
+  --       async = false,
+  --       timeout_ms = 1000,
+  --     },
+  --   }
+  --   vim.keymap.set({ 'n', 'v' }, '<leader>df', function()
+  --     conform.format {
+  --       lsp_fallback = true,
+  --       async = false,
+  --       timeout_ms = 1000,
+  --     }
+  --   end, { desc = 'LSP: [F]ormat File' })
+  -- end,
 }
