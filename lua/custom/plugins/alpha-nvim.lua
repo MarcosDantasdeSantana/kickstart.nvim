d2b4cea479a4d5f5eb1f255a1c1bc0dc2618c320
MarcosDantasdeSantana <billgm2@outlook.com>
2024-04-25 22:18:27 -0300

    [UX] My dashboard

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -6,6 +6,43 @@ return { -- NOTE: Menu inicial
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    require('alpha').setup(require('alpha.themes.dashboard').config)
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.header.val = {
+      [[    ███╗   ███╗ █████╗ ██╗  ██╗███████╗   ]],
+      [[    ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝   ]],
+      [[    ██╔████╔██║███████║█████╔╝ █████╗     ]],
+      [[    ██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝     ]],
+      [[    ██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗   ]],
+      [[    ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ]],
+      [[      ██████╗ ██████╗  ██████╗ ██╗        ]],
+      [[     ██╔════╝██╔═══██╗██╔═══██╗██║        ]],
+      [[     ██║     ██║   ██║██║   ██║██║        ]],
+      [[     ██║     ██║   ██║██║   ██║██║        ]],
+      [[     ╚██████╗╚██████╔╝╚██████╔╝███████╗   ]],
+      [[      ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝   ]],
+      [[███████╗████████╗██╗   ██╗███████╗███████╗]],
+      [[██╔════╝╚══██╔══╝██║   ██║██╔════╝██╔════╝]],
+      [[███████╗   ██║   ██║   ██║█████╗  █████╗  ]],
+      [[╚════██║   ██║   ██║   ██║██╔══╝  ██╔══╝  ]],
+      [[███████║   ██║   ╚██████╔╝██║     ██║     ]],
+      [[╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝     ]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button('f', ' ' .. ' Find file', ':Telescope find_files <CR>'),
+      dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles <CR>'),
+      dashboard.button('g', ' ' .. ' Find text', ':Telescope live_grep <CR>'),
+      dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
+      dashboard.button('s', ' ' .. ' Restore Session', ':SessionManager load_last_session<CR>'),
+      dashboard.button('l', '󰒲 ' .. ' Lazy', ':Lazy<CR>'),
+    }
+    local handle = io.popen 'fortune'
+    local fortune = handle:read '*a'
+    handle:close()
+    dashboard.section.footer.val = fortune
+
+    dashboard.config.opts.noautocmd = true
+    alpha.setup(dashboard.config)
   end,
 }
